3ab5da3ca30248aaa616b1264038d2f859062bbb
Dan Victor <danvictorprogramming1@gmail.com>
2021-12-31 04:19:47 +0800

    Completed the Rock Paper Scissor Script

--- rps.js
+++ rps.js
@@ -1,1 +1,43 @@
-console.log("Hello World");
+function computerPlay() {
+	if ((Math.floor(Math.random() * 2)) == 0) {
+		return "rock";
+	} else if ((Math.floor(Math.random() * 2)) == 1) {
+		return "paper";
+	} else {
+		return "scissor";
+	}
+}
+
+
+function playRound(playerSelection,computerPlay) {
+	if (playerSelection.toLowerCase() == computerPlay) {
+		return "Draw";
+	} else if (playerSelection.toLowerCase() == "rock") {
+		if (computerPlay == "paper") {
+			return "You lose";
+		} else if (computerPlay == "scissor") {
+			return "You Win";
+		}
+	} else if (playerSelection.toLowerCase() == "paper") {
+		if (computerPlay == "scissor") {
+			return "You lose";
+		} else if (computerPlay == "rock") {
+			return "You Win";
+		}
+	} else if (playerSelection.toLowerCase() == "scissor") {
+		if (computerPlay == "rock") {
+			return "You lose";
+		} else if (computerPlay == "paper") {
+			return "You win";
+		}
+	}
+}
+function game() {
+	let playerSelection = prompt("Rock Paper Scissor").toLowerCase();
+	alert(playRound(playerSelection,computerPlay()));
+}
+game();
+game();
+game();
+game();
+game();
