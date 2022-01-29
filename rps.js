7b9a0303693115d548d3a1ab29f818b0b7231751
Danvictorgithub <danvictorprogramming1@gmail.com>
2022-01-30 06:19:01 +0800

    0.1 DOM UI Improvement

--- rps.js
+++ rps.js
@@ -1,3 +1,4 @@
+//Generates Computer Choice
 function computerPlay() {
 	if ((Math.floor(Math.random() * 2)) == 0) {
 		return "rock";
@@ -7,30 +8,101 @@ function computerPlay() {
 		return "scissor";
 	}
 }
+// Plays Rock-Paper-Scissor Logic and Score Update Implementation
 function playRound(playerSelection,computerPlay) {
-	if (playerSelection.toLowerCase() == computerPlay) {
-		return "Draw";
-	} else if (playerSelection.toLowerCase() == "rock") {
+	if (playerSelection == computerPlay) {
+		console.log('draw');
+		uiText = 'draw' ;
+		resultInfo.innerHTML = `${uiText}`;
+	} else if (playerSelection == "rock") {
 		if (computerPlay == "paper") {
-			return "You lose";
+			computerScore += 1;
+			ComputerScoreInfo.textContent = `${computerScore}`;
+			uiText = 'You lose';
+			resultInfo.innerHTML = `${uiText}`;
+			wincheck();
 		} else if (computerPlay == "scissor") {
-			return "You Win";
+			playerScore += 1;
+			playerScoreInfo.textContent = `${playerScore}`;
+			uiText = 'You Win';
+			resultInfo.innerHTML = `${uiText}`;
+			wincheck();
 		}
-	} else if (playerSelection.toLowerCase() == "paper") {
+	} else if (playerSelection == "paper") {
 		if (computerPlay == "scissor") {
-			return "You lose";
+			computerScore += 1;
+			ComputerScoreInfo.textContent = `${computerScore}`;
+			uiText = 'You lose';
+			resultInfo.innerHTML = `${uiText}`;
+			wincheck();
 		} else if (computerPlay == "rock") {
-			return "You Win";
+			playerScore += 1;
+			playerScoreInfo.textContent = `${playerScore}`;
+			uiText = 'You Win';
+			resultInfo.innerHTML = `${uiText}`;
+			wincheck();
 		}
-	} else if (playerSelection.toLowerCase() == "scissor") {
+	} else if (playerSelection == "scissor") {
 		if (computerPlay == "rock") {
-			return "You lose";
+			computerScore += 1;
+			ComputerScoreInfo.textContent = `${computerScore}`;
+			uiText = 'You lose';
+			resultInfo.innerHTML = `${uiText}`;
+			wincheck();
 		} else if (computerPlay == "paper") {
-			return "You win";
+			playerScore += 1;
+			playerScoreInfo.textContent = `${playerScore}`;
+			uiText = 'You Win';
+			resultInfo.innerHTML = `${uiText}`;
+			wincheck();
+		}
+	} else {
+		uiText = 'Something is Wrong';
+		resultInfo.innerHTML = `${uiText}`;
+	}
+	console.log(playerScore);
+}
+//Selects Player choice by attribute, Computer choice generation, and Winner Logic
+function game(e) {
+	let playerSelection = e.target.getAttribute('value');
+	console.log(playerSelection)
+	if (isEnd) {
+		return;
+	} else {
+		playRound(playerSelection,computerPlay());
+	}
+	
+}
+function wincheck() {
+	if (playerScore >= 5 || computerScore >= 5) {
+		isEnd = true;
+		if (playerScore > computerScore) {
+			uiText = 'Player Wins!';
+			resultInfo.innerHTML = `${uiText}`;
+		} else {
+			uiText = 'Computer Wins!';
+			resultInfo.innerHTML = `${uiText}`;
 		}
 	}
 }
-function game() {
-	let playerSelection = prompt("Rock Paper Scissor").toLowerCase();
-	alert(playRound(playerSelection,computerPlay()));
-}
+
+// Player and Computer Info Stats
+let playerScore = 0;
+let computerScore = 0;
+let uiText = 'Select Your Weapon';
+let playerChoice = 'none';
+let computerChoice = 'none';
+let isEnd = false;
+// DOM Selectors for UI and Buttons
+buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+	button = document.addEventListener('click', game);
+	});
+playerScoreInfo = document.querySelector('.player-score');
+playerScoreInfo.textContent = `${playerScore}`;
+
+ComputerScoreInfo = document.querySelector('.computer-score');
+ComputerScoreInfo.textContent = `${computerScore}`;
+
+resultInfo = document.querySelector('.result-info');
+resultInfo.innerHTML = `${uiText}`;
